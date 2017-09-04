7cc5f1cded5a0e39d96e3360d5adb0a6629c04a3
Mario Cho <hephaex@gmail.com>
2017-09-05 02:01:36 +0900

    numbers

---
+++ notebooks/myapp.py
@@ -0,0 +1,10 @@
+print(type(1))                 #1
+## <class 'int'>
+print(isinstance(1, int))      #2
+## True
+print(1 + 1)                   #3
+## 2
+print(1 + 1.0)                 #4
+## 2.0
+print(type(2.0))
+## <class 'float'>

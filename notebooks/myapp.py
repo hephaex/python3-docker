403b1ab9c166d1cb87f069414e1ac502d6c9c2a2
Mario Cho <hephaex@gmail.com>
2017-09-05 02:22:13 +0900

    if

--- notebooks/myapp.py
+++ notebooks/myapp.py
@@ -1,8 +1,10 @@
-i = 0
-while i < 10:
+# for 변수 in 데이터가 있다면:
+#   실행
+# for i in range(0, 10):
+for i in range(10):
     if i == 5:
-        break
+        # break
+        continue
     print(i)
-    i += 1
 else:
     print("end")

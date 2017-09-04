bf726afcb43613e4bdb18c1b7642ede3f7a24402
Mario Cho <hephaex@gmail.com>
2017-09-05 02:24:10 +0900

    function

--- notebooks/myapp.py
+++ notebooks/myapp.py
@@ -1,10 +1,13 @@
-# for 변수 in 데이터가 있다면:
-#   실행
-# for i in range(0, 10):
-for i in range(10):
-    if i == 5:
-        # break
-        continue
-    print(i)
-else:
-    print("end")
+# def say_hi():
+#     print("hi")
+#
+# say_hi()
+
+# def say_hi(name, age):
+def say_hi(name, age = 20):
+    print("hi {0} ({1})".format(name, age))
+
+say_hi("tom", 23)   # tom (23)
+say_hi("bob", 21)   # bob (21)
+say_hi("steve")     # steve (20)
+say_hi(age = 18, name = "rick")
